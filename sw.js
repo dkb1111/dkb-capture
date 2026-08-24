5e620b9f63c3ed0e8e09599cb8c2d9eef562f457
dkb1111 <desbrowndarwin1@gmail.com>
2026-08-24 11:14:06 +0930

    Update service worker cache version and optimize code

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE='dkb-capture-v03';
-const ASSETS=['./','index.html','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-  ).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+const CACHE='dkb-capture-v030';const ASSETS=['./','index.html','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
