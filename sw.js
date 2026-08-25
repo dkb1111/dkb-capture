a53e5cf752d106df0636788ad3b7fbf8253c5540
dkb1111 <desbrowndarwin1@gmail.com>
2026-08-25 12:16:23 +0930

    Update cache version from v034 to v035

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dkb-capture-v034';
+const CACHE='dkb-capture-v035';
 const ASSETS=['./','index.html','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
